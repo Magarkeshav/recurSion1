3abb7a3c3c77b884f20aa3a2715a532541620fe4
keshav-magar <keshavmagar233@gmail.com>
2026-07-18 11:28:42 +0530

    stringrecurction

--- src/skiptthecharacter.java
+++ src/skiptthecharacter.java
@@ -1,22 +1,23 @@
 public class skiptthecharacter {
 
-    public static  void skip(String ns , String cs){
+    public static String  skip(String ns , String cs){
         if(cs.isEmpty()){
-            System.out.println(ns);
-            return;
+
+            return "";
 
         }
         char ch = cs.charAt(0);
 
         if(ch=='a' || ch=='A'){
-            skip(ns,cs.substring(1));
+           return skip(ns,cs.substring(1));
         }else{
-            skip(ns+ch,cs.substring(1));
+           return ch + skip(ns+ch,cs.substring(1));
         }
     }
     public static void main(String[] args) {
         String str = "ckdssalmsaAlv";
         String str1 = "";
-        skip(str1,str);
+        String str2 = skip(str1,str);
+        System.out.println(str2);
     }
 }
